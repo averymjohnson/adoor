1252817308959f8a8ac6bc32135cf89ea7fd8d92
Nick Herrera <nickherrera34@gmail.com>
2018-01-17 23:10:09 -0600

    add

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 641feb4675b589f9f0fd1c9a97652f2149346a2d
 // Set Handlebars
 var exphbs = require('express-handlebars');
 
@@ -21,6 +17,6 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them
-var routes = require('./controllers/adoor-controllers.js');
+var routes = require('./routes/html-routes.js');
 
 app.listen(port);
