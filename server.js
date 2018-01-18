0745bb51eafbfcf2d77c95faf9fd463b3be3d64f
Nick Herrera <nickherrera34@gmail.com>
2018-01-17 19:22:58 -0600

    added controllers file

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+// var methodOverride = require('method-override');//idk
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyparser.json());
+
+// app.use(methodOverride('_method'));
+
+// Set Handlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them
+var routes = require('./controllers/adoor_controllers.js');
+
+app.use('/', routes);
+
+app.listen(port);
