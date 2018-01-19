abc97181374375b59011fa74ce11396949443946
Nick Herrera <nickherrera34@gmail.com>
2018-01-18 19:12:51 -0600

    updated routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,28 +11,31 @@ module.exports = function(app) {
   // index/home page
   app.get("/", function(req, res) {
     // res.sendFile(path.join(__dirname, ""));
-    console.log("home page is working");
+    console.log("home page load successful");
   });
 
   // log-in page
   app.get("/log-in", function(req, res) {
     // res.sendFile(path.join(__dirname, ""));
-    console.log("Log-In is working");
+    console.log("log in page load successful");
   });
 
   // Bueyr Create a Profile Page/Fill out Survey
   app.get("/profile-create", function(req, res) {
     // res.sendFile(path.join(__dirname, ""));
+    console.log("profile create page load successful");
   });
 
   // Matching Page
   app.get("/matching", function(req, res) {
     // res.sendFile(path.join(__dirname, ""));
+    console.log("matching page load successful");
   });
 
   // My Matches Page
   app.get("/my-matches", function(req, res) {
     // res.sendFile(path.join(__dirname, ""));
+    console.log("my matches page load successful");
   });
 
   
@@ -43,16 +46,19 @@ module.exports = function(app) {
  // Seller Create Profile
   app.get("/admin-create", function(req, res) {
     // res.sendFile(path.join(__dirname, ""));
+    console.log("admin create page load successful");
   });
 
   // Upload New Listing Page
   app.get("/add-listing", function(req, res) {
     // res.sendFile(path.join(__dirname, ""));
+    console.log("add listing page load successful");
   });
 
   //Manage Listings Page 
   app.get("/manage-listings", function(req, res) {
     // res.sendFile(path.join(__dirname, ""));
+    console.log("manage listing page load successful");
   });
 
 
