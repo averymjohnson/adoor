85a64cdab4bbeda6347e98d19040ec196c72b9da
Nick Herrera <nickherrera34@gmail.com>
2018-01-20 11:52:32 -0600

    added handlebars files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,32 +9,32 @@ module.exports = function(app) {
 // =============================================================
 
   // index/home page
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, ""));
-    console.log("home page load successful");
+  app.get('/', function (req, res) {
+    res.render('index');
+    console.log("working");
   });
 
   // log-in page
-  app.get("/log-in", function(req, res) {
-    // res.sendFile(path.join(__dirname, ""));
+  app.get('/log-in', function (req, res) {
+    res.render('adoor');;
     console.log("log in page load successful");
   });
 
   // Bueyr Create a Profile Page/Fill out Survey
-  app.get("/profile-create", function(req, res) {
-    // res.sendFile(path.join(__dirname, ""));
+  app.get('/profile-create', function (req, res) {
+    res.render('adoor');;
     console.log("profile create page load successful");
   });
 
   // Matching Page
-  app.get("/matching", function(req, res) {
-    // res.sendFile(path.join(__dirname, ""));
+  app.get('/matching', function (req, res) {
+    res.render('adoor');;
     console.log("matching page load successful");
   });
 
   // My Matches Page
-  app.get("/my-matches", function(req, res) {
-    // res.sendFile(path.join(__dirname, ""));
+  app.get('/my-matches', function (req, res) {
+    res.render('adoor');;
     console.log("my matches page load successful");
   });
 
@@ -44,20 +44,20 @@ module.exports = function(app) {
  // ==========================================================
 
  // Seller Create Profile
-  app.get("/admin-create", function(req, res) {
-    // res.sendFile(path.join(__dirname, ""));
+  app.get('/admin-create', function (req, res) {
+    res.render('adoor');;
     console.log("admin create page load successful");
   });
 
   // Upload New Listing Page
-  app.get("/add-listing", function(req, res) {
-    // res.sendFile(path.join(__dirname, ""));
+  app.get('/new-listing', function (req, res) {
+    res.render('adoor');;
     console.log("add listing page load successful");
   });
 
   //Manage Listings Page 
-  app.get("/manage-listings", function(req, res) {
-    // res.sendFile(path.join(__dirname, ""));
+  app.get('/manage-listings', function (req, res) {
+    res.render('adoor');;
     console.log("manage listing page load successful");
   });
 
