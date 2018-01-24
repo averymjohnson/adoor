1ccb56c81f290a43ccef5b51cce4b3d8a96bcb85
Avery Johnson <averymjohnson@outlook.com>
2018-01-23 23:00:52 -0600

    added upload photo and createlisting.js

--- public/js/createlisting.js
+++ public/js/createlisting.js
@@ -1,35 +1,46 @@
-// img upload code
-$(document).ready( function() {
-      $(document).on('change', '.btn-file :file', function() {
-    var input = $(this),
-      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [label]);
-    });
+$(document).ready(function() {
+  // Getting jQuery references to the responses
+  var addressInput = $("#address");
+  var cityInput = $("#city");
+  var zipInput = $("#zipcode");
+  var sqftInput = $("#sqft");
+  var bedInput = $("#bed");
+  var bathInput = $("#bath");
+  var priceInput = $("#price");
+  var imgInput = $("#img");
 
-    $('.btn-file :file').on('fileselect', function(event, label) {
-        
-        var input = $(this).parents('.input-group').find(':text'),
-            log = label;
-        
-        if( input.length ) {
-            input.val(log);
-        } else {
-            if( log ) alert(log);
-        }
-      
-    });
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            
-            reader.onload = function (e) {
-                $('#img-upload').attr('src', e.target.result);
-            }
-            
-            reader.readAsDataURL(input.files[0]);
-        }
+  // Adding an event listener for when the form is submitted
+  $("#submitListing").on("submit", handleFormSubmit);
+
+  // A function for handling what happens when the form to create a new post is submitted
+  function newUser(event) {
+    event.preventDefault();
+    // Wont submit the post if we are missing a body, title, or author
+    if (!addressInput.val().trim() || !cityInput.val().trim() || !zipInput.val() || !sqftInput.val() || !bedInput.val() || !bathInput.val() || !priceInput.val() || !imgInput.val()) {
+      return;
     }
-
-    $("#imgInp").change(function(){
-        readURL(this);
-    }); 
+    // Constructing a newAcct object to hand to the database
+    var newAcct = {
+      address: addressInput
+        .val()
+        .trim(),
+      city: cityInput
+        .val()
+        .trim(),
+      zip: zipInput
+        .val()
+        .trim(),
+      sqft: sqftInput
+        .val()
+        .trim(),
+      bed: bedInput
+        .val()
+        .trim(),
+      bath: bathInput
+        .val()
+        .trim(),
+      price: priceInput
+        .val()
+        .trim(),
+      img: imgInput
+    };
