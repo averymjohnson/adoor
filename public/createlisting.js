13705a289b6136d966d7d79b1ccc6690113c0106
Avery Johnson <averymjohnson@outlook.com>
2018-01-23 19:03:44 -0600

    updated

--- public/createlisting.js
+++ public/createlisting.js
@@ -1,45 +1,58 @@
- var mysql = require("mysql"); 
+//  var mysql = require("mysql"); 
 
  
- 
- var connection = mysql.createConnection({ 
-   host: "localhost", 
-   port: 3306, 
-   user: "root", 
-   password: "2203", 
-   database: "bamazon" 
- }); 
 
+//  var connection = mysql.createConnection({ 
+//    host: "localhost", 
+//    port: 3306, 
+//    user: "root", 
+//    password: "2203", 
+//    database: "adoor_db" 
+//  }); 
 
-connection.connect(function(err){ 
- 	console.log("Connected to Bamazon Manager"); 
- 	runSearch(); 
- 	}); 
-  
+//  $("#submit").click(function(){
 
+// connection.connect(function(err){ 
+//  	alert("Connected"); 
 
+//  	}); 
+// });
 $("#submit").click(function(){
 
-	var responses = {
-		"address" = $("#address").val().trim(),
-		"city" = $("#city").val().trim(),
-		"zipCode" = $("#city").val().trim(),
-		"bed" = $("#bed").val().trim(),
-		"bath" = $("#bath").val().trim(),
-		"price" = $("#price").val().trim(),
-		"img" = $("#imginp)
-		};
 
-	$.then(function(){
-		var listing = [[responses.address, responses.city, responses.zipCode, responses.bed, responses.bath, responses.price, responses.img]];
-		var query = "INSERT INTO adoor_db.listings (address, city, zipCode, bed, bath, price, img) VALUES ?"
-		connection.query(query, [listing], function(err, res){
-			alert("Listing is Added");
-		});
+	var addressR = $("#address").val().trim();
+	var cityR = $("#city").val().trim();
+	var zipCodeR = $("#zip").val().trim();
+	var bedR = $("#bed").val().trim(); 
+	var bathR = $("#bath").val().trim();
+	var priceR = $("#price").val().trim();
+	var imgR = $("#imginp");
 
-	connection.end();
 
-	}):
-}:
+	var responses = [
+	{
+		"address" = addressR,
+		"city" = cityR,
+		"zipCode" = zipCodeR,
+		"bed" = bedR,
+		"bath" = bathR,
+		"price" = priceR,
+		"img" = imgR
+			}
+		];
+
+	module.exports = responses;
+
+// 	$.then(function(){
+// 		var listing = [[responses.address, responses.city, responses.zipCode, responses.bed, responses.bath, responses.price, responses.img]];
+// 		var query = "INSERT INTO adoor_db.listings (address, city, zipCode, bed, bath, price, img) VALUES ?"
+// 		connection.query(query, [listing], function(err, res){
+// 			alert("Listing is Added");
+// 		});
+
+// 	connection.end();
+
+// 	})
+// }
 
 
